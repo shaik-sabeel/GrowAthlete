d66ff8ead58c2e61dc7b167541b6de106da9ec8d
Muhammed Ikram <ikramuk812@gmail.com>
2025-08-25 22:43:36 +0530

    added filter functionality to the athletes page

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -163,4 +163,18 @@ router.get("/profile", verifyToken, async(req, res) => {
   res.status(200).json({ user });
 });
 
+router.get("/profile/:id", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
 module.exports = router;
